ba6f713c1c9cffe292328269e07497ca114c1cc7
Izzatbek <abdusharipovizzat03@gmail.com>
2025-09-08 16:24:00 +0500

    commerce

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Search, ShoppingBasket, UserRound } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed top-0 left-0 w-full flex items-center justify-between px-4 sm:px-6 md:px-8 lg:px-30 py-4 gap-2 bg-white z-50'>
+      <div className="text-2xl font-semibold text-green-600">Logo</div>
+      <div className="flex items-center gap-3">
+        <Link>Home</Link>
+        <Link>Menu</Link>
+        <Link>Contact Us</Link>
+      </div>
+      <div className="flex items-center gap-3 lg:gap-5">
+        <Search/>
+        <ShoppingBasket/>
+        <UserRound/>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,67 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  const navigate=useNavigate();
-  
-  useEffect(()=>{
-    if(!localStorage.getItem("accessToken")){
-        navigate("/login")
-      }
-  },[navigate]);
-  return (
-    <div>Home</div>
-  )
-}
+  const navigate = useNavigate();
+  const menuImages = [
+    { name: "Salad", image: "/menu_1.png" },
+    { name: "Rolls", image: "/menu_2.png" },
+    { name: "Deserts", image: "/menu_3.png" },
+    { name: "Sandwich", image: "/menu_4.png" },
+    { name: "Cake", image: "/menu_5.png" },
+    { name: "Pure Veg", image: "/menu_6.png" },
+    { name: "Pasta", image: "/menu_7.png" },
+    { name: "Noodles", image: "/menu_8.png" },
+  ];
 
-export default Home
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="w-full max-w-6xl mx-auto pt-24 text-white relative">
+        <img src="/header_img.png" alt="header image" className="w-full" />
+        <div className="absolute top-1/3 left-10">
+          <h2 className="text-3xl lg:text-5xl font-semibold">Order your</h2>
+          <h2 className="text-3xl lg:text-5xl font-semibold pb-3">
+            favourite food here
+          </h2>
+          <p className="max-w-md pb-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+            aperiam corrupti dolores temporibus sunt soluta numquam.
+          </p>
+          <button className="bg-white text-black px-5 py-2.5 rounded-full shadow cursor-pointer hover:bg-gray-50 transition duration-300">
+            View Menu
+          </button>
+        </div>
+      </div>
+
+      <div className="w-full max-w-6xl mx-auto mt-10">
+        <h1 className="text-2xl font-semibold pb-2">Explore Our Menu</h1>
+        <p className="text-sm text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+
+        <div className="flex items-center justify-between gap-6 mt-6">
+          {menuImages.map((item, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <img
+                src={item.image}
+                alt={item.name}
+                className="w-24 h-24 object-contain"
+              />
+              <p className="mt-2 font-medium">{item.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
