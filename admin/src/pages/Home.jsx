138a428c52ba13ff725516e80f8e94a670968d85
Izzatbek <abdusharipovizzat03@gmail.com>
2025-09-12 14:17:29 +0500

    commerce

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,15 +1,43 @@
 import { X } from 'lucide-react';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import axiosInstance from '../api/axiosInstance';
 
 const Home = () => {
       const navigate = useNavigate();
+      const [products,setProducts]=useState([]);
+
+      const getAllProducts = async ()=>{
+        try {
+          const {data} = await axiosInstance.get("/products/data/all");
+          setProducts(data);
+        } catch (error) {
+          toast.error(error.message || "Network Error");
+        }
+      }
+
+      const deleteProduct = async (id)=>{
+        try {
+          const {data} = await axiosInstance.delete(`/products/product/delete/${id}`);
+          if(data){
+            toast.success(data || "Deleted successfully");
+          }
+        } catch (error) {
+          toast.error(error?.message || "Network Error");
+        }
+      }
 
     useEffect(()=>{
         if(!localStorage.getItem("accessToken")){
             navigate("/login");
         }
     },[navigate]);
+
+    useEffect(()=>{
+      getAllProducts();
+    },[]);
+
   return (
     <>
     <div className="w-full min-h-screen mx-auto padd">
@@ -26,13 +54,18 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className='border bg-gray-50'>
-              <td className='px-4 py-3'><img src="./food_1.png" alt="food" className='w-12 h-12 object-cover rounded-md' /></td>
-              <td className='px-4 py-3'>Grek Food</td>
-              <td className='px-4 py-3'>Salad</td>
-              <td className='px-4 py-3'>12$</td>
-              <td className='px-4 py-3 cursor-pointer text-gray-300 hover:text-gray-500 transition duration-300'><X size={20} /></td>
-            </tr>
+            {
+              products.map((product)=>(
+                <tr className='border bg-gray-50' key={product?.id}>
+                      <td className='px-4 py-3'><img src={`http://localhost:8080${product?.image}`} alt={product?.title} className='w-12 h-12 object-cover rounded-md' /></td>
+                      <td className='px-4 py-3'>{product?.title}</td>
+                      <td className='px-4 py-3'>{product?.category}</td>
+                      <td className='px-4 py-3'>{product?.price}$</td>
+                      <td className='px-4 py-3 cursor-pointer text-gray-300 hover:text-gray-500 transition duration-300'
+                       onClick={()=>deleteProduct(product?.id)}><X size={20} /></td>
+                 </tr>
+              ))
+            }
           </tbody>
         </table>
       </div>
